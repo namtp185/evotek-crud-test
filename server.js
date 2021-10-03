e7cf19a62078101cae0d8f62c25ecee3272e43e0
namtp185 <namtp185@gmail.com>
2021-10-03 22:38:51 +0700

    refactor

--- server.js
+++ server.js
@@ -3,14 +3,17 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const cors = require("cors");
+const routes = require('./routers/index');
+const userRoute = require("./routers/user");
+const connectDB = require("./dbConnector");
+const seedData = require("./dbSeeder");
+
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const connectDB = require("./dbConnector");
 connectDB();
-const seedData = require("./dbSeeder");
 seedData()
   .then((res) => {
     console.log("Database create successfully!");
@@ -24,8 +27,8 @@ app.set('view engine','pug');
 app.set('views','./views');
 
 app.use(express.static('./public'));
-app.use(require('./routers/index'));
-app.use('/users', require("./api/user.controller"));
+app.use(routes);
+app.use('/users', userRoute);
 
 const errorHandler = require("./routers/errorHandler");
 app.use(errorHandler);
